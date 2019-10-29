e74029e82cb4b1178b47d97afc89282cfd1db141
nanders0 <nikolaus.g.anderson@gmail.com>
2019-10-29 13:58:36 -0400

    added downloads for schema and dictionary
    Former-commit-id: 1804cb2adeff21f1af703f5cab34d54e794a46fc

--- ginas/api/api.controller.js
+++ ginas/api/api.controller.js
@@ -1,7 +1,7 @@
 angular.module('ginas')
 .controller('apiController',function ($scope, apiFactory, $http) {
                 $scope.isLoading = false;
-                $scope.url = $scope.swaggerUrl = 'https://tripod.nih.gov/ginas/gsrs/gsrs.json';
+                $scope.url = $scope.swaggerUrl = 'gsrs/gsrs.json';
 
                 $scope.myErrorHandler = function(message, code){
                     console.log(code);
